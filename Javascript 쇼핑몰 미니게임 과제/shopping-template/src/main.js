386efb8eebdf01bd2e29f45998c2956859cf2807
soozzang <soozzang@inha.deu>
2023-01-04 16:44:44 +0900

    JavaScript - handling events

--- Javascript 쇼핑몰 미니게임 과제/shopping-template/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/shopping-template/src/main.js
@@ -24,6 +24,25 @@ function createHTMLString(item) {
     `;
 }
 
+function onButtonClick(event, items) {
+    const key = dataset.key;
+    const dataset = event.target.dataset;
+    const value = dataset.value;
+    
+    if(key == null || value == null) {
+        return;
+    }
+    updateitems (items, key, value);
+}
+
+
+function setEventlistners(items) {
+    const logo = document.querySelector('.logo');
+    const buttons = document.querySelector('.buttons');
+    logo.addEventListener('click', () => displayItems(items));
+    buttons.addEventListener('click', event => onButtonClick(event, items));
+}
+
 // main
 loadItems()
     .then(items=> {
