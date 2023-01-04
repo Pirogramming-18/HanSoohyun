4e0cb51fa61afe9961b5696d3cb5b7fd83d38a30
soozzang <soozzang@inha.deu>
2023-01-04 16:57:24 +0900

    JavaScript - fetch

--- Javascript 쇼핑몰 미니게임 과제/shopping-template/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/shopping-template/src/main.js
@@ -14,32 +14,3 @@ function displayItems(items) {
 
 }
 
-//  Create HTML list item from the given data item //
-function createHTMLString(item) {
-    return `
-    <li class="item">
-        <img src="${item.image}" alt="${item.type}" class="item_thumbnail">
-        <span class="item_description">${item.gender}, ${item.size}</span>
-    </li>
-    `;
-}
-
-function onButtonClick(event, items) {
-    const key = dataset.key;
-    const dataset = event.target.dataset;
-    const value = dataset.value;
-    
-    if(key == null || value == null) {
-        return;
-    }
-    updateitems (items, key, value);
-}
-
-
-function setEventlistners(items) {
-    const logo = document.querySelector('.logo');
-    const buttons = document.querySelector('.buttons');
-    logo.addEventListener('click', () => displayItems(items));
-    buttons.addEventListener('click', event => onButtonClick(event, items));
-}
-
