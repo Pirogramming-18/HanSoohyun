f6411bcd84e0d4c2e1c385ce8de0ad6a41ef8a28
soozzang <soozzang@inha.deu>
2023-01-06 14:48:07 +0900

    1-8

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,49 +1,42 @@
-num=0;
-flag=0
-while (True):
-    a=input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
-    try:
-        a = int(a)        
-    except:
-        print("정수를 입력하세요")
-        continue
-    if (type(a) != int):
-        print("정수를 입력하세요")
-    elif(a <= 0 or a > 3):
-        print("1,2,3 중 하나를 입력하세요")
-    else:
+global num
+num=0
+import random
+def brGame(X,Y):
+        for i in range(a):
+            global num
+            global flag
+            num += 1
+            if num==31:
+                print(f"{Y} win!")
+                flag+=1
+                break
+            R=f'{X} : {num}'
+            print(R)
+        
+
+while True:
+    global flag
+    flag=0
+    while (True):
+        a=input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+        try:
+            a = int(a)        
+        except:
+            print("정수를 입력하세요")
+            continue
+        if (type(a) != int):
+            print("정수를 입력하세요")
+        elif(a <= 0 or a > 3):
+            print("1,2,3 중 하나를 입력하세요")
+        else:
+            break
+    
+    brGame(X="playerA",Y="playerB")
+    if flag==1:
+        break
+        
+    
+    brGame(X="playerB",Y="playerA")
+    if flag==1:
         break
     
-for i in range(a):
-    num += 1
-    if num==31:
-        print(f"PlayerB win!")
-        flag+=1
-        break
-    R=f'PlayerA : {num}'
-    print(R)
-
-while (True):
-    a=input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
-    try:
-        a = int(a)        
-    except:
-        print("정수를 입력하세요")
-        continue
-    if (type(a) != int):
-        print("정수를 입력하세요")
-    elif(a <= 0 or a > 3):
-        print("1,2,3 중 하나를 입력하세요")
-    else:
-        break    
-
-for i in range(a):
-    num += 1
-    if num==31:
-        print(f"PlayerA win!")
-        flag+=1
-        break
-    R=f'PlayerB : {num}'
-    print(R)
-    
-   
