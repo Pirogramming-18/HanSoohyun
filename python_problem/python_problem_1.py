f3df41f8f05a08a64f2ada61d9f511158c104b68
soozzang <soozzang@inha.deu>
2023-01-06 14:43:15 +0900

    1-5

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -17,5 +17,24 @@ for i in range(a):
     num += 1
     R=f'PlayerA : {num}'
     print(R)
+
+while (True):
+    a=input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+    try:
+        a = int(a)        
+    except:
+        print("정수를 입력하세요")
+        continue
+    if (type(a) != int):
+        print("정수를 입력하세요")
+    elif(a <= 0 or a > 3):
+        print("1,2,3 중 하나를 입력하세요")
+    else:
+        break    
+
+for i in range(a):
+    num += 1
+    R=f'PlayerB : {num}'
+    print(R)
     
    
